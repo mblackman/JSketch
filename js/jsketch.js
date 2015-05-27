97d3fed3eafc5808d25804e3e0aa8012a99dc25b
Matt Blackman <mateoblackman@gmail.com>
2015-05-26 21:05:29 -0700

    Page has basic functionality now

--- js/jsketch.js
+++ js/jsketch.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-	
+	generateGrid(16);
 });
+
+function generateGrid(size) {
+	
+	$("#etch").empty();
+	$("#etch").css("width", size * 20 + "px");
+	$("#etch").css("height", size * 20 + "px");
+
+	for (i = 0; i < size; i++) {
+		var row = document.createElement("div");
+		row.className = "row";
+
+		for (j = 0; j < size; j++) {
+			var temp = document.createElement("div");
+			temp.className = "square";
+			//temp.id = "R" + i + " C" + j;
+			$("#etch").append(temp);
+		}
+	}
+
+	$("div.square").hover(function() {
+		$(this).addClass("hover");
+	});
+}
+
+function clickClear() {
+	var gridSize = prompt("Enter size of new grid", "16");
+	generateGrid(gridSize);
+}
